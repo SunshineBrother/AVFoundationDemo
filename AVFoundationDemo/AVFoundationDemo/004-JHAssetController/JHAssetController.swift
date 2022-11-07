7b0f1d7b65fdd097196654d4fdf9f7d832b23780
Sunshine <2806139037@qq.com>
2022-11-07 15:02:19 +0800

    feat：添加AVAsset：资源&元数据处理

---
+++ AVFoundationDemo/AVFoundationDemo/004-JHAssetController/JHAssetController.swift
@@ -0,0 +1,20 @@
+//
+//  JHAssetController.swift
+//  AVFoundationDemo
+//
+//  Created by sunshine on 2022/11/7.
+//
+
+import UIKit
+import AVKit
+class JHAssetController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+         
+    }
+
+ 
+}
